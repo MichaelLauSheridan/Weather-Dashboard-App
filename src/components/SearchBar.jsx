7fca2866e6c84c91c4a9e6211d7d135c93103696
Michael Lau <point_zero25@hotmail.com>
2026-04-13 23:17:29 -0400

    Created searchbar jsx

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,22 @@
+function SearchBar({ city, setCity, onSearch }) {
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') {
+            onSearch()
+        }
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Enter city name"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <button onClick={onSearch}>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar
